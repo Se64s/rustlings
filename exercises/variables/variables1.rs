fb4da41de33765651e303255d2604de95e422ea2
Sebastián Del Moral <sebmorgal@gmail.com>
2022-08-29 21:49:36 +0200

    feat: Add first row of resolved exercises

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -2,9 +2,13 @@
 // Make me compile!
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    x = 5;
-    println!("x has the value {}", x);
+    let x = 5;
+    println!("x has the value {x}");
+    let x = 6;
+    println!("x has the value {x}");
+    let mut x = 6;
+    println!("x has the value {x}");
+    x = 7;
+    println!("x has the value {x}");
 }
